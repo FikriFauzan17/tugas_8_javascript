50aa7e9ffdcfcb949203544df5214f268a79d8e4
User-Niomic-Fikri <fikrifauzan17@gmail.com>
2020-04-21 16:10:13 +0700

    Tugas 8 JavaScript
    tugas javascript 8

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function object(){
+  var smartphone = {
+    merk: 'Samsung',
+    harga: 2000000,
+    ukuranlayar: 6,
+    tahunkeluaran: [2018,2019,2020]
+  }
+  smartphone.ukuranlayar = 5.5;
+  console.log(smartphone);
+  console.log(smartphone.ukuranlayar);
+}
+object();
